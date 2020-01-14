e539b62dbaa65c05b9a07518eaf9a7af7fee14d2
Riley Slayden <slaydenriley@gmail.com>
2020-01-14 16:30:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id=nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
 end
